b6031ecce562ab3996ee30ad425a06c313ff649a
Matias Safranchik <matisafra@hotmail.com>
2024-12-14 20:52:37 -0300

    add one more use users test

--- src/containers/transactionsContainer/__tests__/useUsers.test.ts
+++ src/containers/transactionsContainer/__tests__/useUsers.test.ts
@@ -2,16 +2,17 @@ import { renderHook, waitFor } from '@testing-library/react'
 import { useUsers } from '../useUsers'
 import { AllTheProviders } from 'utils/testUtils'
 import users from 'mockData/users'
+import Router from 'react-router-dom'
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
-  useParams: () => ({
-    id: '1',
-  }),
+  useParams: jest.fn(),
 }))
 
 describe('useUsers', () => {
   it('should return the users filtered by the planet id', async () => {
+    jest.spyOn(Router, 'useParams').mockReturnValue({ id: '1' })
+
     const { result } = renderHook(() => useUsers(), {
       wrapper: AllTheProviders,
     })
@@ -23,5 +24,16 @@ describe('useUsers', () => {
         users: users.filter((user) => user.homeworld === '1'),
       })
     )
+  })
+
+  it('should not call the query if the planet id is not provided', async () => {
+    jest.spyOn(Router, 'useParams').mockReturnValue({})
+
+    const { result } = renderHook(() => useUsers(), {
+      wrapper: AllTheProviders,
+    })
+
+    await waitFor(() => expect(result.current.isLoading).toBe(false))
+    expect(result.current.data).not.toBeDefined()
   })
 })
